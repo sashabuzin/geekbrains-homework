8f8fb4bc5fb051530b64c9bce58209ba4610a638
sashabuzin <n9159648714@gmail.com>
2021-03-23 13:36:34 +0300

    practice4

--- com.geekbrains.buzin.android/src/lesson2/main.java
+++ com.geekbrains.buzin.android/src/lesson2/main.java
@@ -1,31 +1,51 @@
 package lesson2;
 
-import java.lang.reflect.Array;
+
 
 public class main {
     public static void main(String[] args) {
+    fillDiagonal();
+    }
 
-        int[] practice1 = {1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-        for (int i = 0; i < practice1.length; i++) {
-            if (practice1[i] == 1) {
-                practice1[i] = 0;
-            } else practice1[i] = 1;
+    public static void invertArray() {
+        int[] array = {1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 1) {
+                array[i] = 0;
+            } else array[i] = 1;
         }
+    }
 
-        int[] practice2 = new int[8];
+    public static void fillArray() {
+        int[] array = new int[8];
         int counterToIncreaseTheValueBy3 = 0;
-        for (int i = 0; i < practice2.length; i++) {
-            practice2[i] = counterToIncreaseTheValueBy3;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = counterToIncreaseTheValueBy3;
             counterToIncreaseTheValueBy3 += 3;
         }
+    }
 
-        int[] practice3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        for (int i = 0; i < practice3.length; i++) {
-            if (practice3[i] < 6) {
-                practice3[i] *= 2;
+    public static void changeArray() {
+        int[] array = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 6) {
+                array[i] *= 2;
             }
         }
-
     }
+
+    public static void fillDiagonal() {
+        int[][] array = new int[4][4];
+        for (int i = 0; i < array.length; i++){
+            for (int j = 0; j < 4; j++){
+                if (((i == 0 || i == 3) && (j == 0 || j == 3)) || ((i == 1 || i == 2) && (j == 1 || j == 2))){
+                    array[i][j] = 1;
+                }
+            }
+        }
+    }
+
+
+
 
 }
