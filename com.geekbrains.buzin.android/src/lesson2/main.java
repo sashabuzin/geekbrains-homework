1c5c0516532d68581daf82dd374a4c3dca1829ce
sashabuzin <n9159648714@gmail.com>
2021-03-23 12:22:29 +0300

    practice2

--- com.geekbrains.buzin.android/src/lesson2/main.java
+++ com.geekbrains.buzin.android/src/lesson2/main.java
@@ -2,12 +2,19 @@ package lesson2;
 
 public class main {
     public static void main(String[] args) {
+
         int[] practice1 = {1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         for (int i = 0; i < practice1.length; i++) {
             if (practice1[i] == 1) {
                 practice1[i] = 0;
             } else practice1[i] = 1;
         }
+
+        int[] practice2 = new int[8];
+        int counterToIncreaseTheValueBy3 = 0;
+        for (int i = 0; i < practice2.length; i++)
+            practice2[i] = counterToIncreaseTheValueBy3;
+            counterToIncreaseTheValueBy3 += 3;
     }
 
 }
