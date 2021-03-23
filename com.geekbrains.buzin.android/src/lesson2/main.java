12bc02a821573a873deb4ec7c5866f56200cb8b0
sashabuzin <n9159648714@gmail.com>
2021-03-23 12:08:52 +0300

    practice1

--- com.geekbrains.buzin.android/src/lesson2/main.java
+++ com.geekbrains.buzin.android/src/lesson2/main.java
@@ -1,5 +1,13 @@
 package lesson2;
 
 public class main {
+    public static void main(String[] args) {
+        int[] practice1 = {1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        for (int i = 0; i < practice1.length; i++) {
+            if (practice1[i] == 1) {
+                practice1[i] = 0;
+            } else practice1[i] = 1;
+        }
+    }
 
 }
