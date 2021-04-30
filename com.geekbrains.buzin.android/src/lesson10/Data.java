c9360be953d67b503b45044af94e53eaaa055d07
sashabuzin <n9159648714@gmail.com>
2021-04-30 17:21:51 +0300

    Lesson 10 is finished.

---
+++ com.geekbrains.buzin.android/src/lesson10/Data.java
@@ -0,0 +1,12 @@
+package lesson10;
+
+import java.util.Map;
+
+public class Data {
+    public static String[] dataArray = {"Яблоко","Апельсин","Яблоко","Вишня","Банан","Манго",
+            "Вишня","Кокос","Клубника","Арбуз","Черника","Дыня"};
+
+
+
+}
+
--- com.geekbrains.buzin.android/src/lesson10/Main.java
+++ com.geekbrains.buzin.android/src/lesson10/Main.java
@@ -1,4 +1,24 @@
 package lesson10;
 
+import java.util.*;
+
 public class Main {
+    public static void main(String[] args) {
+        List<String> arrayList = new ArrayList<>(Arrays.asList(Data.dataArray));
+        Set<String> uniqueElements = new HashSet<>(arrayList);
+        Map<String, Integer> countingRepetitions = new HashMap<>();
+        System.out.println("Входной массив слов: " + arrayList);
+        System.out.println("Массив уникальных слов: " + uniqueElements);
+
+        for (String word : arrayList) {
+            Integer val = countingRepetitions.get(word);
+                if (val == null) {
+                    countingRepetitions.put(word, 1);
+                } else {
+                    countingRepetitions.put(word, val + 1);
+                }
+        }
+        System.out.println("Подсчет повторений слов в первоначальном массиве: " + countingRepetitions);
+
+    }
 }
